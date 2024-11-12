65f789276261023c9101bf7438acff837918f6c9
nuruldini <93868161+nuruldini@users.noreply.github.com>
2024-11-12 14:38:49 +0800

    Update ayamsuper.py

--- ayamsuper.py
+++ ayamsuper.py
@@ -1,15 +1,15 @@
+import streamlit as st
 import pandas as pd
 
 # Load the dataset from the CSV file
 file_path = 'https://raw.githubusercontent.com/nuruldini/Assignment/refs/heads/main/lookup_premise.csv'
 df = pd.read_csv(file_path)
 
-df
+# Display the DataFrame using Streamlit
 st.write(df)
 
-# Filter the DataFrame to include only rows where the 'state' column is 'Pahang'
-df_NS= df[df['state'] == 'Negeri Sembilan']
+# Filter the DataFrame to include only rows where the 'state' column is 'Negeri Sembilan'
+df_NS = df[df['state'] == 'Negeri Sembilan']
 
-df_NS
-
-
+# Display the filtered DataFrame
+st.write(df_NS)
