6c50834880cc508e8d2a94998a41d488297b15ee
nuruldini <93868161+nuruldini@users.noreply.github.com>
2024-11-12 14:43:26 +0800

    Update ayamsuper.py

--- ayamsuper.py
+++ ayamsuper.py
@@ -5,12 +5,14 @@ import pandas as pd
 file_path = 'https://raw.githubusercontent.com/nuruldini/Assignment/refs/heads/main/lookup_premise.csv'
 df = pd.read_csv(file_path)
 
-
-# Display the DataFrame without the first column using Streamlit
+# Display the DataFrame
 st.write(df)
 
 # Filter the DataFrame to include only rows where the 'state' column is 'Negeri Sembilan'
 df_NS = df[df['state'] == 'Negeri Sembilan']
 
-# Display the filtered DataFrame
+# Reset the index and create a new column with sequential numbers for 'bill number'
+df_NS['bill number'] = range(1, len(df_NS) + 1)
+
+# Display the updated filtered DataFrame
 st.write(df_NS)
