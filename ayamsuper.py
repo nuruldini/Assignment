0941add3d01698fbafbcd542ebd8889f26b9013a
nuruldini <93868161+nuruldini@users.noreply.github.com>
2024-11-12 14:46:58 +0800

    Update ayamsuper.py

--- ayamsuper.py
+++ ayamsuper.py
@@ -5,12 +5,11 @@ import pandas as pd
 file_path = 'https://raw.githubusercontent.com/nuruldini/Assignment/refs/heads/main/lookup_premise.csv'
 df = pd.read_csv(file_path)
 
-# Display the DataFrame
-st.write(df)
-
 # Filter the DataFrame to include only rows where the 'state' column is 'Negeri Sembilan'
 df_NS = df[df['state'] == 'Negeri Sembilan']
 
+# Reset the index and drop the old index column
+df_NS_reset = df_NS.reset_index(drop=True)
 
-# Display the updated filtered DataFrame
-st.write(df_NS)
+# Display the updated DataFrame without the old index
+st.write(df_NS_reset)
